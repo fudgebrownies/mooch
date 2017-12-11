664257c75ed634fb2051e5df2d7549d9e617f868
hornd24 <david.horn689@gmail.com>
2017-12-11 12:51:31 -0800

    'seeds

--- seeders/seeds.sql
+++ seeders/seeds.sql
@@ -5,5 +5,5 @@ VALUES('mooch','sell','cartman.jpg','714-467-9086','1967 raybands Newport,CA 948
 
 
 INSERT INTO products(username,category,product_name,product_description,userUploadImage1, userUploadImage2, daily, weekly, monthly, security_deposit)
-VALUES('mooch','tools','Drill','a wierless handled drill','drill1.jpg','drill2.jpg',8,15,50,20)
+VALUES('mooch','tools','Drill','a wierless handled drill','drill1.jpg','drill2.jpg',8,15,50,20),('mooch','sports','football','football to throw','footbal1.jpg','football2.jpg',10,14,50,8)
 
